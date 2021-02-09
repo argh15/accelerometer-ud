472f171ecc6549b967a67d8a15596b4a51edbc97
Arghadeep <archakraborty@teksystems.com>
2021-02-09 12:28:31 +0530

    Accelerometer Data used.

--- Accelerometer-UD/Accelerometer-UD/ViewController.swift
+++ Accelerometer-UD/Accelerometer-UD/ViewController.swift
@@ -6,12 +6,39 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var motionManager: CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+    }
+    
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data  else {
+            print(error.debugDescription)
+            return
+        }
+        
+        xLabel.text = "X: \(getNumberFormatter().string(for: accelerometerData.acceleration.x)!)"
+        yLabel.text = "Y: \(getNumberFormatter().string(for: accelerometerData.acceleration.y)!)"
+        zLabel.text = "Z: \(getNumberFormatter().string(for: accelerometerData.acceleration.z)!)"
+    }
+    
+    func getNumberFormatter() -> NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        return formatter
     }
 
 
